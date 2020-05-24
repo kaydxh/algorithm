9611f1dd78c1984214dfa7c101f42321172d87e3
kayxhding <kayxhding@tencent.com>
2020-05-24 23:17:08 +0800

    [CHG] change code comment

--- leetcode/cpp/153_findMinimumInRotatedSortedArray/findMinimumInRotatedSortedArray.cpp
+++ leetcode/cpp/153_findMinimumInRotatedSortedArray/findMinimumInRotatedSortedArray.cpp
@@ -29,17 +29,17 @@ using namespace std;
 class Solution {
  public:
   /*
-1 有序数组分成了左右2个小的有序数组，而实际上要找的是右边有序数组的最小值
-2 如果中间值大于右边的最大值，说明中间值还在左边的小数组里，需要更新left =
-middle + 1 3 否则就是中间值小于等于当前右边最大值，mid
-已经在右边的小数组里了，但是至少说明了当前右边的right值不是最小值了或者不是唯一的最小值，需要慢慢向左移到middle。
-4. 主要这里因为right = num.size() - 1, 本来while（left <=
-right）,因为3里right=middle，所以为了避免死循环，改为while(left <
-right)，例子[3,1,2]
+  1 有序数组分成了左右2个小的有序数组，而实际上要找的是右边有序数组的最小值
+  2 如果中间值大于右边的最大值，说明中间值还在左边的小数组里，需要更新left =
+  middle + 1 3 否则就是中间值小于等于当前右边最大值，mid
+  已经在右边的小数组里了，但是至少说明了当前右边的right值不是最小值了或者不是唯一的最小值，需要慢慢向左移到middle。
+  4. 主要这里因为right = num.size() - 1, 本来while（left <=
+  right）,因为3里right=middle，所以为了避免死循环，改为while(left <
+  right)，例子[3,1,2]
 
-复杂度：
-时间复杂度：O(logn)
-空间复杂度：O(1)
+  复杂度：
+  时间复杂度：O(logn)
+  空间复杂度：O(1)
 */
   int findMin(vector<int>& nums) {
     int left = 0;
