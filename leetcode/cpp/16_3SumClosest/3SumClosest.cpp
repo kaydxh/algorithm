f743e057b53460a8722de961d2929ca0cee81235
kayxhding <kayxhding@tencent.com>
2022-02-10 12:32:02 +0800

    fix(3sumclosest): array size 3, fix sum  0

--- leetcode/cpp/16_3SumClosest/3SumClosest.cpp
+++ leetcode/cpp/16_3SumClosest/3SumClosest.cpp
@@ -21,56 +21,56 @@ The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
 using namespace std;
 
 class Solution {
- public:
-  /*
-  思路：排序+双指针法
-  1. 对数组进行排序
-  2. 遍历current 从 0 ～ nums.size() - 1
-  3. low指针指向index 为1， high指向nums.size() - 1
-  4. sum = nums[current] + nums[low] + nums[high],
-    如果 sum = target,返回sum，如果，sum < target, ++low,否则 --high
+   public:
+    /*
+    思路：排序+双指针法
+    1. 对数组进行排序
+    2. 遍历current 从 0 ～ nums.size() - 1
+    3. low指针指向index 为1， high指向nums.size() - 1
+    4. sum = nums[current] + nums[low] + nums[high],
+      如果 sum = target,返回sum，如果，sum < target, ++low,否则 --high
 
-  复杂度：
-  1. 时间复杂度O(n2)
-  2. 空间复杂度O(1)
-*/
-  int threeSumClosest(vector<int>& nums, int target) {
-    if (nums.size() < 3) {
-      return 0;
-    }
-
-    sort(nums.begin(), nums.end());
-
-    int result = nums[0] + nums[1] + nums[2];
-    int closestValue = abs(target - result);
-    int sum = 0;
-    for (auto current = 0; current < nums.size() - 2; ++current) {
-      int low = current + 1;
-      int high = nums.size() - 1;
-
-      while (low < high) {
-        sum = nums[current] + nums[low] + nums[high];
-        if (sum == target) {
-          return sum;
-        } else if (sum < target) {
-          ++low;
-        } else {
-          --high;
+    复杂度：
+    1. 时间复杂度O(n2)
+    2. 空间复杂度O(1)
+  */
+    int threeSumClosest(vector<int>& nums, int target) {
+        if (nums.size() < 3) {
+            return 0;
         }
 
-        if (abs(target - sum) < closestValue) {
-          closestValue = abs(target - sum);
-          result = sum;
+        sort(nums.begin(), nums.end());
+
+        int result = nums[0] + nums[1] + nums[2];
+        int closestValue = abs(target - result);
+        int sum = result;
+        for (auto current = 0; current < nums.size() - 2; ++current) {
+            int low = current + 1;
+            int high = nums.size() - 1;
+
+            while (low < high) {
+                sum = nums[current] + nums[low] + nums[high];
+                if (sum == target) {
+                    return sum;
+                } else if (sum < target) {
+                    ++low;
+                } else {
+                    --high;
+                }
+
+                if (abs(target - sum) < closestValue) {
+                    closestValue = abs(target - sum);
+                    result = sum;
+                }
+            }
         }
-      }
+        return result;
     }
-    return result;
-  }
 };
 
 int main() {
-  vector<int> nums = {-1, 2, 1, -4};
-  Solution sol;
-  auto result = sol.threeSumClosest(nums, 1);
-  cout << result << endl;
+    vector<int> nums = {-1, 2, 1, -4};
+    Solution sol;
+    auto result = sol.threeSumClosest(nums, 1);
+    cout << result << endl;
 }
