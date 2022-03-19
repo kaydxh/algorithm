48cd32f3b42c7a124efac45c2429e72c6f413e51
kayxhding <kayxhding@tencent.com>
2022-03-19 15:53:03 +0800

    feat(unique path): dynamic programming

--- leetcode/cpp/62_uniquePaths/uniquePaths.cpp
+++ leetcode/cpp/62_uniquePaths/uniquePaths.cpp
@@ -35,36 +35,41 @@ It's guaranteed that the answer will be less than or equal to 2 * 10 ^ 9.
 using namespace std;
 
 class Solution {
- public:
-  /*
- 动态规划：
- 1. p[i][j] 是到达 i, j 最多路径
- 2. 动态方程：dp[i][j] = dp[i-1][j] +
- dp[i][j-1]（到达第i行第j列的路径数=到达i-1行第j列的路径数 +
- 到达第i行第j-1列的路径数） 注意，对于第一行 dp[0][j]，或者第一列
- dp[i][0]，由于都是在边界，所以只能为 1(只需要初始化dp[0][1]或者dp[1][0]即可)
+   public:
+    /*
+   动态规划：
+   1. dp[i][j] 是到达 i, j 最多路径
+   2. 动态方程：dp[i][j] = dp[i-1][j] +
+   dp[i][j-1]（到达第i行第j列的路径数=到达i-1行第j列的路径数 +
+   到达第i行第j-1列的路径数） 注意，对于第一行 dp[0][j]，或者第一列
+   dp[i][0]，由于都是在边界，所以只能为 1
 
- 复杂度：
- 时间复杂度：O(m*n)
- 空间复杂度：O(m * n)
- 优化：因为我们每次只需要 dp[i-1][j],dp[i][j-1]
- 所以我们只要记录这两个数
-*/
-  int uniquePaths(int m, int n) {
-    vector<vector<int>> dp(m + 1, vector<int>(n + 1));
-    dp[0][1] = 1;
-    for (int i = 1; i <= m; ++i) {
-      for (int j = 1; j <= n; ++j) {
-        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-      }
+   复杂度：
+   时间复杂度：O(m*n)
+   空间复杂度：O(m * n)
+   优化：因为我们每次只需要 dp[i-1][j],dp[i][j-1]
+   所以我们只要记录这两个数
+  */
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n));
+        for (int i = 0; i < m; ++i) {
+            dp[i][0] = 1;
+        }
+        for (int j = 0; j < n; ++j) {
+            dp[0][j] = 1;
+        }
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        return dp[m - 1][n - 1];
     }
-
-    return dp[m][n];
-  }
 };
 
 int main() {
-  Solution sol;
-  auto result = sol.uniquePaths(7, 3);
-  cout << result << endl;
+    Solution sol;
+    auto result = sol.uniquePaths(7, 3);
+    cout << result << endl;
 }
