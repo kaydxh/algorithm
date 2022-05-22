1b8d0e10adf7c87407a851ee49de813d30c0f196
kaydxh <xiao_hua_ding@sina.cn>
2022-05-22 21:09:26 +0800

    feat(template): add 0-1 problem for dynamic programming

--- template/dynamic_programming.cpp
+++ template/dynamic_programming.cpp
@@ -38,3 +38,34 @@ int dynamic(vector<int> &nums) {
     return dp[nums.size() - 1];
 }
 
+/*
+ * 选与不选问题
+ * dp[i][j]
+ */
+bool partition(vector<int> &nums, int target) {
+    if (nums.empty()) {
+        return false;
+    }
+    vector<vector<int>> dp(nums.size(), vector<int>(target + 1));
+    //二维数组大小为 nums.size(), target+1
+    if (nums[0] <= target) {
+        dp[0][nums[0]] = true;
+    }
+    for (int i = 1; i < nums.size(); ++i) {
+        for (int j = 0; j <= target; ++j) {
+            if (nums[i] == target) {
+                dp[i][j] = true;
+                continue;
+            }
+
+            // 默认不选, 当j - nums[i] < 0时， 只能不选择位置j
+            dp[i][j] = dp[i - 1][j];
+            // 二维数组索引需要满足>= 0
+            if (j - nums[i] >= 0) {
+                dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i]];  //不选 || 选
+            }
+        }
+    }
+
+    return dp[nums.size() - 1][target];
+}
